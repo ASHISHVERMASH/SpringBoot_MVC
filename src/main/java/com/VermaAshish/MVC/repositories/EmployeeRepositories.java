bebdf6bcc02e786b41c76b110b4298df33646da9
ASHISHVERMASH <160701608+ASHISHVERMASH@users.noreply.github.com>
2025-09-05 15:15:56 +0530

    Persistence Api

---
+++ src/main/java/com/VermaAshish/MVC/repositories/EmployeeRepositories.java
@@ -0,0 +1,10 @@
+package com.VermaAshish.MVC.repositories;
+
+import com.VermaAshish.MVC.entities.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepositories extends JpaRepository<EmployeeEntity , Long> {
+
+}
